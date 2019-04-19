47628b4970e49624f8f9c731f87af8d813bd1fd9
EC2 Default User <ec2-user@ip-172-31-31-178.ap-southeast-1.compute.internal>
2019-04-19 07:29:01 +0000

    Add tokenize for + -

--- ecc.c
+++ ecc.c
@@ -1,34 +1,107 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_TOKEN 255
+
+// Token type
+enum {
+  TK_NUM = 256, // Integer token
+  TK_EOF,       // End of token
+};
+
+//Token struct
+typedef struct {
+  int ty;      // Token type
+  int val;     // Token value (integer)
+  char *input; // Token character (for error)
+} Token;
+
+// Array for tokenized token
+Token tokens[MAX_TOKEN];
+
+// Separate p(char) with space and set to tokens
+void tokenize(char *p) {
+  int i = 0;
+  while (*p) {
+    // Skip space
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      tokens[i].ty = *p;
+      tokens[i].input = p;
+      i++;
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      tokens[i].ty = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+
+    fprintf(stderr, "Failed to tokenize: %s\n", p);
+    exit(1);
+  }
+
+  tokens[i].ty = TK_EOF;
+  tokens[i].input = p;
+}
+
+// Output error and exit program
+void error(int i) {
+  fprintf(stderr, "Invalid token: %s\n",
+          tokens[i].input);
+  exit(1);
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "Invalid arg number\n");
+    fprintf(stderr, "Invalid args number\n");
     return 1;
   }
 
-  char *p = argv[1];
+  tokenize(argv[1]);
 
+  // Output first half assembly
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+  // First character must be digit
+  if (tokens[0].ty != TK_NUM) error(0);
+  printf("  mov rax, %d\n", tokens[0].val);
+
+  // Token must be `+ <digit>` or `- <digit>`
+  // Output assembly
+  int i = 1;
+  while (tokens[i].ty != TK_EOF) {
+    if (tokens[i].ty == '+') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error(i);
+      printf("  add rax, %d\n", tokens[i].val);
+      i++;
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+    if (tokens[i].ty == '-') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error(i);
+      printf("  sub rax, %d\n", tokens[i].val);
+      i++;
       continue;
     }
 
-    fprintf(stderr, "Invalid character: '%c'\n", *p);
-    return 1;
+    error(i);
   }
 
   printf("  ret\n");
